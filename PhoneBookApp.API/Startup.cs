fa0cc884325d45a21174a8401a72a645a20806f1
Bongani <bongani@comcorp.co.za>
2021-02-13 22:01:04 +0200

    Connection string fix

--- PhoneBookApp.API/Startup.cs
+++ PhoneBookApp.API/Startup.cs
@@ -12,12 +12,14 @@ namespace PhoneBookApp.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _connPath = env.ContentRootPath;
         }
 
         public IConfiguration Configuration { get; }
+        string _connPath;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,14 +34,20 @@ namespace PhoneBookApp.API
 
             services.AddDependencyInjection();
 
+            string conn = Configuration["ConnectionStrings:PhoneBookDBContext"];
+            if (conn.Contains("%CONTENTROOTPATH%"))
+            {
+                conn = conn.Replace("%CONTENTROOTPATH%", _connPath);
+            }
             services.AddDbContext<EfDbContext>(options =>
-                options.UseSqlServer(Configuration["ConnectionStrings:PhoneBookDBContext"]));
+                options.UseSqlServer(conn));
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            _connPath = env.ContentRootPath;
             if (env.IsDevelopment())
             {
                 app.UseSwaggerDocumentation();
